e170e04e03d1b8b764b81580334f8c8dff70239f
Vinícius Fraga <vinifragam@gmail.com>
2022-09-01 16:32:34 -0300

    CSS-in-JS - Biblioteca de ícones

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,21 @@
-import { Container, Logo } from "./styles";
+import { BackButton, BackIcon, Container, Logo } from "./styles";
 
 import logoImg from '@assets/logo.png';
 
-export function Header() {
+type Props = {
+  showBackButton?: boolean;
+}
+
+export function Header({ showBackButton = false }: Props) {
   return (
     <Container>
+      { 
+        showBackButton &&
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      }
+
       <Logo source={logoImg} />
     </Container>
   )
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components/native";
+import { CaretLeft } from "phosphor-react-native";
 
 export const Container = styled.View`
   width: 100%;
@@ -12,3 +13,12 @@ export const Logo = styled.Image`
   width: 46px;
   height: 55px;
 `;
+
+export const BackButton = styled.TouchableOpacity`
+  flex: 1;
+`;
+
+export const BackIcon = styled(CaretLeft).attrs(({ theme }) => ({
+  size: 36,
+  color: theme.COLORS.WHITE
+}))``;
