93018577bab39263a00a3a95a17c069191494215
Vinícius Fraga <vinifragam@gmail.com>
2022-09-01 16:21:42 -0300

    Estrutura do projeto - Path Mapping

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Groups } from './src/screens/Groups';
+import { Groups } from '@screens/Groups';
 
 export default function App() {
   return (
--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@routes': './src/routes',
+            '@screens': './src/screens',
+            '@storage': './src/storage',
+            '@theme': './src/theme',
+            '@utils': './src/utils',
+          }
+        }
+      ]
+    ]
   };
 };
