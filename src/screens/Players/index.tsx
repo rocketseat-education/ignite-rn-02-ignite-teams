2a865a8adeea18f1e2e02b8eb9fc07f426767b09
Rennan <rplaykl@gmail.com>
2022-09-02 15:57:11 -0300

    CSS-in-JS - Listando os filtros

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,12 +1,18 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native'
+
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
-
-import { Container, Form } from "./styles";
-import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
+import { Input } from "@components/Input";
+
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
+  const [team, setTeam] = useState('Time A')
+  const [players, setPlayers] = useState([])
+
   return (
     <Container>
       <Header showBackButton />
@@ -27,10 +33,24 @@ export function Players() {
         />
       </Form>
 
-      <Filter 
-        title="Time A"
-        isActive
-      />
+      <HeaderList>
+        <FlatList 
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter 
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumberOfPlayers>
+          {players.length}
+        </NumberOfPlayers>
+      </HeaderList>
     </Container>
   )
 }
--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
@@ -15,4 +15,20 @@ export const Form = styled.View`
   justify-content: center;
 
   border-radius: 6px;
+`;
+
+export const HeaderList = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  align-items: center;
+  margin: 32px 0 12px;
+`;
+
+export const NumberOfPlayers = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_200};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.SM}px;
+  `};
 `;
