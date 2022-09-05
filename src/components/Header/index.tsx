07f1cb8249f4cf2debd05f6d33f5d567f30d2869
Rennan <rplaykl@gmail.com>
2022-09-05 12:43:49 -0300

    Navegação - Implementando o voltar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from "@react-navigation/native";
+
 import { BackButton, BackIcon, Container, Logo } from "./styles";
 
 import logoImg from '@assets/logo.png';
@@ -7,11 +9,18 @@ type Props = {
 }
 
 export function Header({ showBackButton = false }: Props) {
+
+  const navigation = useNavigation()
+
+  function handleGoBack() {
+    navigation.navigate('groups')
+  }
+
   return (
     <Container>
       { 
         showBackButton &&
-        <BackButton>
+        <BackButton onPress={handleGoBack}>
           <BackIcon />
         </BackButton>
       }
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from "@react-navigation/native";
+
 import { Container, Content, Icon } from "./styles";
 
 import { Header } from "@components/Header";
@@ -6,6 +8,13 @@ import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 
 export function NewGroup() {
+
+  const navigation = useNavigation()
+
+  function handleNew() {
+    navigation.navigate('players', { group: 'Rocket' })
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -25,6 +34,7 @@ export function NewGroup() {
         <Button 
           title="Criar"
           style={{ marginTop: 20 }}
+          onPress={handleNew}
         />
       </Content>
     </Container>
