312807486b34d5204df6df87b04f03009f37f49b
Rennan <rplaykl@gmail.com>
2022-09-05 12:52:27 -0300

    Navegação - Utilizando a SafeAreaView

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,6 +1,7 @@
+import { SafeAreaView } from 'react-native-safe-area-context'
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   padding: 24px;
--- src/screens/NewGroup/styles.ts
+++ src/screens/NewGroup/styles.ts
@@ -1,7 +1,8 @@
-import { UsersThree } from "phosphor-react-native";
+import { SafeAreaView } from 'react-native-safe-area-context'
 import styled from "styled-components/native";
+import { UsersThree } from "phosphor-react-native";
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   padding: 24px;
