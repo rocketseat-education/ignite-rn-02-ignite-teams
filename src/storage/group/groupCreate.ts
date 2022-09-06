2c6db4707a29d4f105e7ae682e3977e976562521
Rennan <rplaykl@gmail.com>
2022-09-06 18:15:37 -0300

    Local Storage - Criando uma classe de erros personalizada

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,4 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AppError } from '@utils/AppError';
 
 import { groupsGetAll } from './groupsGetAll';
 import { GROUP_COLLECTION } from '@storage/storageConfig';
@@ -7,8 +8,15 @@ export async function groupCreate(newGroup: string) {
   try {
     const storedGroups = await groupsGetAll();
 
+    const groupAlreadyExists = storedGroups.includes(newGroup);
+
+    if(groupAlreadyExists) {
+      throw new AppError('Já existe um grupo cadastrado com esse nome.')
+    }
+
     const storage = JSON.stringify([...storedGroups, newGroup])
     await AsyncStorage.setItem(GROUP_COLLECTION, storage);
+    
   } catch (error) {
     throw error;
   }
---
+++ src/utils/AppError.ts
@@ -0,0 +1,7 @@
+export class AppError {
+  message: string;
+
+  constructor(message: string) {
+    this.message = message;
+  }
+}
