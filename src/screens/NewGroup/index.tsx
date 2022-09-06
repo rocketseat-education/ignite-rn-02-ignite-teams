c008c5f7d8354d6d9d28c3bb1274a36069314764
Rennan <rplaykl@gmail.com>
2022-09-06 18:28:10 -0300

    Local Storage - Tratando exceções

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { Alert } from "react-native";
 
+import { AppError } from "@utils/AppError";
 import { groupCreate } from "@storage/group/groupCreate";
 
 import { Container, Content, Icon } from "./styles";
@@ -11,16 +13,26 @@ import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 
 export function NewGroup() {
-  const [group, setGroup] = useState('')
+  const [group, setGroup] = useState('');
 
-  const navigation = useNavigation()
+  const navigation = useNavigation();
 
   async function handleNew() {
     try {
+
+      if(group.trim().length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma.');
+      }
+
       await groupCreate(group);
-      navigation.navigate('players', { group })
+      navigation.navigate('players', { group });
     } catch (error) {
-      console.log(error)
+      if(error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message);
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.');
+        console.log(error);
+      }
     }
   }
 
